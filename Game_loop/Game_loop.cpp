ee54b027765b268911fd053afb00b0f56600a0df
Grant Rostig <grant.github@emailmeinfo.com>
2021-04-29 16:38:57 -0500

    Add project files.

---
+++ Game_loop/Game_loop.cpp
@@ -0,0 +1,12 @@
+﻿// Game_loop.cpp : Defines the entry point for the application.
+//
+
+#include "Game_loop.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
